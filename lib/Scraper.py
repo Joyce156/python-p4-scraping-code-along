85438f5e064f32aac0130732565d6e2f9003970c
Joyce Njogu <joycenjogu127@gmail.com>
2026-01-13 16:23:51 +0300

    completed lab

--- lib/Scraper.py
+++ lib/Scraper.py
@@ -1,35 +1,37 @@
 from bs4 import BeautifulSoup
-import requests
-from Course import Course
-import ipdb
+
+# projects: kickstarter.select("li.project.grid_4")[0]
+# title: project.select("h2.bbcard_name strong a")[0].text
+# image link: project.select("div.project-thumbnail a img")[0]['src']
+# description: project.select("p.bbcard_blurb")[0].text
+# location: project.select("ul.project-meta span.location-name")[0].text
+# percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+
+def create_project_dict():
+    html = ''
+    with open('./fixtures/kickstarter.html') as file:
+        html = file.read()
+
+    kickstarter = BeautifulSoup(html, 'html.parser')
+    projects = {}
+
+    # Iterate through all projects
+    for project in kickstarter.select("li.project.grid_4"):
+        title = project.select("h2.bbcard_name strong a")[0].text.strip()
+
+        projects[title] = {
+            "image_link": project.select("div.project-thumbnail a img")[0]["src"],
+            "description": project.select("p.bbcard_blurb")[0].text.strip(),
+            "location": project.select("ul.project-meta span.location-name")[0].text.strip(),
+            "percent_funded": project.select(
+                "ul.project-stats li.first.funded strong"
+            )[0].text.replace("%", "").strip()
+        }
+
+    return projects
 
 
-
-class Scraper:
-    def __init__(self):
-        self.courses = []
-
-    def get_page(self):
-        doc = BeautifulSoup(
-            requests.get("http://learn-co-curriculum.github.io/site-for-scraping/courses").text,
-            "html.parser"
-        )
-        return doc
-
-    def get_courses(self):
-        return self.get_page().select(".post")
-
-    def make_courses(self):
-        for course in self.get_courses():
-            title = course.select("h2")[0].text if course.select("h2") else ""
-            schedule = course.select(".date")[0].text if course.select(".date") else ""
-            description = course.select("p")[0].text if course.select("p") else ""
-
-            new_course = Course(title, schedule, description)
-            self.courses.append(new_course)
-
-        return self.courses
-
-    def print_courses(self):
-        for course in self.make_courses():
-            print(course)
+# Run the function to test
+if __name__ == "__main__":
+    projects = create_project_dict()
+    print(projects)
